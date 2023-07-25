17463a1eefea416e4fc4122aa26666b8a047d3ac
Juan Posada <juan.posada@astropay.com>
2023-07-25 18:38:05 -0500

    env update

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -1,12 +1,12 @@
 import { error } from '@sveltejs/kit';
 import { getContent, getBuilderSearchParams } from '@builder.io/sdk-svelte';
-import { BUILDER_PUBLIC_API_KEY } from '$env/static/private';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
+	const BUILDER_PUBLIC_API_KEY = process.env.BUILDER_PUBLIC_API_KEY;
 	const content = await getContent({
 		model: 'page',
-		apiKey: BUILDER_PUBLIC_API_KEY,
+		apiKey: BUILDER_PUBLIC_API_KEY as string,
 		options: getBuilderSearchParams(event.url.searchParams),
 		userAttributes: {
 			urlPath: event.url.pathname || '/'
