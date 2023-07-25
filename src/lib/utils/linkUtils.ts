4ed8101ac80d62d6213ed985d8d49a7d563bb275
Juan Posada <juan.posada@astropay.com>
2023-07-25 17:58:12 -0500

    basepath

--- src/lib/utils/linkUtils.ts
+++ src/lib/utils/linkUtils.ts
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png';
+import { base } from '$app/paths';
 
 type Links = {
 	url: string;
@@ -21,48 +22,48 @@ export const navbarData: NavBarData = {
 	logo: true,
 	logoSrc: logo,
 	logoLink: true,
-	linkUrl: '/',
+	linkUrl: `${base}/`,
 	optionalLinkText: 'KitchenKrafts',
 	altText: 'KitchenKrafts Logo',
 	links: [
 		{
-			url: '/',
+			url: `${base}/`,
 			displayInNav: true,
 			displayInFooter: false,
 			linkText: 'Home'
 		},
 		{
-			url: '/about',
+			url: `${base}/about`,
 			displayInNav: true,
 			displayInFooter: false,
 			linkText: 'About us'
 		},
 		{
-			url: '/blog',
+			url: `${base}/blog`,
 			displayInNav: true,
 			displayInFooter: false,
 			linkText: 'Blog'
 		},
 		{
-			url: '/contact',
+			url: `${base}/contact`,
 			displayInNav: true,
 			displayInFooter: true,
 			linkText: 'Contact'
 		},
 		{
-			url: '/privacy-policy',
+			url: `${base}/privacy-policy`,
 			displayInNav: false,
 			displayInFooter: true,
 			linkText: 'Privacy Policy'
 		},
 		{
-			url: '/terms-conditions',
+			url: `${base}/terms-conditions`,
 			displayInNav: false,
 			displayInFooter: true,
 			linkText: 'Terms & Conditions'
 		},
 		{
-			url: '/notFound',
+			url: `${base}/notFound`,
 			displayInNav: false,
 			displayInFooter: false,
 			linkText: ''
