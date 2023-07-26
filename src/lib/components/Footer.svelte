c7120f3cb5f09931b670306eae2cac0623abad9f
Juan Posada <juan.posada@astropay.com>
2023-07-26 12:44:09 -0500

    footer fix

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,12 +7,13 @@
 		FooterLink
 	} from 'flowbite-svelte';
 	import type { NavBarData } from '../utils/linkUtils';
+	import { base } from '$app/paths';
 	export let data: NavBarData;
 </script>
 
 <Footer footerType="logo" class="mt-5">
 	<div class="sm:flex sm:items-center sm:justify-between">
-		<FooterBrand href="/" src={data.logoSrc} alt={data.altText} name={data.optionalLinkText} />
+		<FooterBrand href={base} src={data.logoSrc} alt={data.altText} name={data.optionalLinkText} />
 		<FooterLinkGroup
 			ulClass="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400"
 		>
@@ -24,5 +25,5 @@
 		</FooterLinkGroup>
 	</div>
 	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-	<FooterCopyright href="/" by="KitchenKrafts™" />
+	<FooterCopyright href={base} by="KitchenKrafts™" />
 </Footer>
