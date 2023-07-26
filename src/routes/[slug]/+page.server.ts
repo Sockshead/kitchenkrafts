1c3d26bdbcddbf7ba8d31eff64a8e48590911532
Juan Posada <juan.posada@astropay.com>
2023-07-26 14:56:11 -0500

    possible load from builder.io fix

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,15 +1,13 @@
-import { getBuilderSearchParams, getContent } from '@builder.io/sdk-svelte';
+import { getContent } from '@builder.io/sdk-svelte';
 import { env } from '$env/dynamic/private';
-import { base } from '$app/paths';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
 	const content = await getContent({
 		model: 'page',
 		apiKey: env.BUILDER_PUBLIC_API_KEY,
-		options: getBuilderSearchParams(event.params),
 		userAttributes: {
-			urlPath: event.url.pathname || `${base}/`
+			urlPath: event.url.pathname || `/`
 		}
 	});
 
